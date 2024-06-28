6b97834526b984efbbcea91799103e2aa1c07737
lumi denoir <lumidenoir@gmail.com>
2024-06-28 18:06:28 +0530

    fix: qtile backup config theme name

--- qtile-everforest/autostart_once.sh
+++ qtile-everforest/autostart_once.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Apply wallpaper using wal
-feh --bg-fill ~/Pictures/wallpaper/fog_forest_2.png &&
+feh --bg-fill ~/Pictures/wallpaper/everforest/fog_forest_2.png &
 
 # Start picom
 picom --config ~/.config/picom/picom.conf &
+
+# Start dunst
+dunst &
